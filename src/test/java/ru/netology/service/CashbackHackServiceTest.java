2def32a7f7b7dca291038190dfc57fc0237c6a11
Tatiana <ef.tatiana@mail.ru>
2024-11-24 13:02:53 +0300

    Branch junit4 is added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,34 +3,30 @@ package ru.netology.service;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void TestRemainFor1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount); //вызвали метод remain, он возвращает сумму, на кот. надо докупить
         int expected = 0;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashbackFor900() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        org.testng.Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashbackFor999() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        org.testng.Assert.assertEquals(actual,expected);
+        org.junit.Assert.assertEquals(expected, actual);
     }
-
-
-
-
 }
